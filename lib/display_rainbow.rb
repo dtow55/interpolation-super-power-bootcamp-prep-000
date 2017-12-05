1f399efbce756e4e91dac0236b70a228369d8249
David J Towster <djt4xe@virginia.edu>
2017-12-05 03:57:46 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,5 +4,5 @@ stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
 
 def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
-  puts "Contact: #{person[3]}" 
+  puts "Contact: #{person[3]}"
 end
